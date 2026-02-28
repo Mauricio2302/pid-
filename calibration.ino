c65a8625ddb7954cddfd5eb53b856aa137590968
Mauricio2302 <116759850+Mauricio2302@users.noreply.github.com>
2026-02-27 21:44:49 -0300

    Update calibration settings for all sensors

--- calibration.ino
+++ calibration.ino
@@ -1,51 +1,51 @@
 // ===== SENSOR 1 =====
 void calibrationd(){
-sensor.setBlackMin(10, 25, 10);
-sensor.setBlackMax(20*1.2, 35*1.2, 20*1.2);
+sensor.setBlackMin(3, 10, 3);
+sensor.setBlackMax(100*1.5, 100*1.35, 80*1.15);
 sensor.setGreenMin(0, 100, 0);
 sensor.setGreenMax(50, 255, 50);
 sensor.setRedMin(100, 0, 0);
 sensor.setRedMax(255, 50, 50);
-sensor.setWhiteMin(270*0.9, 230*0.9, 200*0.9);
+sensor.setWhiteMin(140*0.9, 140*0.9, 100*0.9);
 sensor.setWhiteMax(320, 285, 230);
 sensor.setSilverMin(180, 180, 180);
 sensor.setSilverMax(230, 230, 230);
 
 
 // ===== SENSOR 2 =====
-sensor2.setBlackMin(40, 65, 50);
-sensor2.setBlackMax(190*1.2, 120*1.2, 95*1.2);
+sensor2.setBlackMin(30, 45, 25);
+sensor2.setBlackMax(190*1.2, 170*1.2, 95*1.2);
 sensor2.setGreenMin(0, 100, 0);
 sensor2.setGreenMax(50, 255, 50);
 sensor2.setRedMin(100, 0, 0);
 sensor2.setRedMax(255, 50, 50);
-sensor2.setWhiteMin(280*0.9, 320*0.9, 220*0.9);
+sensor2.setWhiteMin(160*0.9, 200*0.9, 120*0.9);
 sensor2.setWhiteMax(440, 465, 345);
 sensor2.setSilverMin(180, 180, 180);
 sensor2.setSilverMax(230, 230, 230);
 
 
 // ===== SENSOR 3 =====
-sensor3.setBlackMin(30, 30, 20);
-sensor3.setBlackMax(160*1.2, 100*1.2, 140*1.2);
+sensor3.setBlackMin(15, 15, 10);
+sensor3.setBlackMax(110*1.6, 100*1.15, 100*1.2);
 sensor3.setGreenMin(0, 100, 0);
 sensor3.setGreenMax(50, 255, 50);
 sensor3.setRedMin(100, 0, 0);
 sensor3.setRedMax(255, 50, 50);
-sensor3.setWhiteMin(220*0.9, 200*0.9, 165*0.9);
+sensor3.setWhiteMin(105*0.9, 110*0.9, 60*0.9);
 sensor3.setWhiteMax(300, 250, 215);
 sensor3.setSilverMin(180, 180, 180);
 sensor3.setSilverMax(230, 230, 230);
 
 
 // ===== SENSOR 4 =====
-sensor4.setBlackMin(80, 120, 50);
-sensor4.setBlackMax(300*1.2, 300*1.2, 220*1.2);
+sensor4.setBlackMin(60, 90, 30);
+sensor4.setBlackMax(300*1.6, 420*1.2, 220*1.2);
 sensor4.setGreenMin(0, 100, 0);
 sensor4.setGreenMax(50, 255, 50);
 sensor4.setRedMin(100, 0, 0);
 sensor4.setRedMax(255, 50, 50);
-sensor4.setWhiteMin(500*0.9, 600*0.9, 360*0.9);
+sensor4.setWhiteMin(390*0.9, 420*0.9, 150*0.9);
 sensor4.setWhiteMax(710, 780, 450);
 sensor4.setSilverMin(180, 180, 180);
 sensor4.setSilverMax(230, 230, 230);
